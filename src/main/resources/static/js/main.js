848b00e382d043e7256575bf55fc0960a015a82f
Allen_Y <626668645@qq.com>
2016-03-28 18:52:37 +0800

    添加海监船的视图模式模块
    根据船的当前实时的定位，完成指北针的功能

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -115,6 +115,7 @@ require([
         "esri/TimeExtent", "esri/dijit/TimeSlider",
         "dojo/_base/array","esri/geometry/geometryEngine", "esri/symbols/SimpleMarkerSymbol",
         "esri/symbols/SimpleFillSymbol","esri/tasks/query",
+        "dojo/dom-geometry","dojo/window","dojo/has",
         "dojo/on",
         "dojo/dom",
         "dojo/domReady!"],
@@ -128,8 +129,19 @@ require([
              Circle,
              Graphic,Point,Popup,InfoTemplate,Draw,lang,Font,TextSymbol,GeometryService,DistanceParameters,
              TimeExtent, TimeSlider, arrayUtils,geometryEngine,SimpleMarkerSymbol,SimpleFillSymbol, Query,
+             domGeom,win,has,
              on,dom
         ) {
+        var pt1,watchId,COMPASS_SIZE = 80,graphic,compassFaceRadius, compassFaceDiameter;
+        var needleAngle, needleWidth, needleLength, compassRing,renderingInterval = -1, currentHeading,hasCompass;
+        var compassHousing,containerX,containerY,compassNeedleContext;
+        var supportsOrientationChange = "onorientationchange" in window,
+            orientationEvent = supportsOrientationChange ? "orientationchange" : "resize";
+
+        window.addEventListener(orientationEvent, function () {
+            orientationChanged();
+        }, false);
+
         loading = dom.byId("loadingImg");
         map = new Map("mapDiv",{logo:false});//加载底图
         tb = new Draw(map);
@@ -149,6 +161,15 @@ require([
         popup.highlight = false;
         popup.titleInBody = false;
         popup.domNode.className += " light";
+        //定义点的填充和半径
+        var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 10,
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                new Color([0,255,0, 0.3]), 10),
+            new Color([0,255,0,1]));
+        var buffSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
+            new SimpleLineSymbol(SimpleLineSymbol.STYLE_LONGDASHDOT,
+                new Color([255,128,0,1]), 3),
+            new Color([255,128,0,0.15]));
         //航线变量定义
         var totalDistance = 0, inputPoints = [], legDistance = [], enableMeasureLength = false;
         //量算服务
@@ -159,17 +180,324 @@ require([
             //增加船体图片
             p.mapPoint = map.extent.getCenter();
             addGraphics(p);
+            //检查地图加载及定位是否支持
+            mapLoadHandler();
+            //船的视图模式compress
+            loadCompass();
             //调用画点划线的方法，台风路径展示
           //  addPath();
         });
-        var pointSymbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 10,
-                          new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
-                          new Color([0,255,0, 0.3]), 10),
-                          new Color([0,255,0,1]));
-        var buffSymbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-                         new SimpleLineSymbol(SimpleLineSymbol.STYLE_LONGDASHDOT,
-                         new Color([255,128,0,1]), 3),
-                         new Color([255,128,0,0.15]));
+        function mapLoadHandler() {
+            console.log("yes in maploadhandler")
+            on(window, 'resize', map, map.resize);
+            //检查是否支持定位
+            if (navigator.geolocation) {
+                navigator.geolocation.getCurrentPosition(zoomToLocation, locationError);
+                // retrieve update about the current geographic location of the device
+                watchId = navigator.geolocation.watchPosition(showLocation, locationError);
+            } else {
+                alert("你的浏览器不支持 Visit http://caniuse.com to discover browser support for the Geolocation API.");
+            }
+        }
+        function zoomToLocation(location) {
+            pt1 = esri.geometry.geographicToWebMercator(new Point(location.coords.longitude, location.coords.latitude));
+            addgraph(pt1);
+            map.centerAndZoom(pt1, 6);
+        }
+
+        function showLocation(location) {
+            pt1 = esri.geometry.geographicToWebMercator(new Point(location.coords.longitude, location.coords.latitude));
+            if (!graphic) {
+                addgraph(pt1);
+            } else {
+                //move the graphic if it already exists
+                graphic.setGeometry(pt1);
+            }
+            map.centerAt(pt1);
+        }
+        function addgraph(pt) {
+            var symbol = new SimpleMarkerSymbol(SimpleMarkerSymbol.STYLE_CIRCLE, 12, new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID, new Color([210, 105, 30, 0.5]), 8), new Color([210, 105, 30, 0.9]));
+            graphic = new Graphic(pt, symbol);
+            map.graphics.add(graphic);
+        }
+        function locationError(error) {
+            //error occurred so stop watchPosition
+            if (navigator.geolocation) {
+                navigator.geolocation.clearWatch(watchId);
+            }
+            switch (error.code) {
+                case error.PERMISSION_DENIED:
+                    alert("Location not provided");
+                    break;
+
+                case error.POSITION_UNAVAILABLE:
+                    alert("Current location not available");
+                    break;
+
+                case error.TIMEOUT:
+                    alert("Timeout");
+                    break;
+
+                default:
+                    alert("unknown error");
+                    break;
+            }
+        }
+        function loadCompass() {
+            console.log("loadcompress");
+            compassHousing = dom.byId("compassHousing");
+            // assign the compass housing dimensions
+            compassHousing.style.height = compassHousing.style.width = COMPASS_SIZE + "px";
+            // return the absolute position of the compass housing
+            containerX = domGeom.position(compassHousing).x;
+            containerY = domGeom.position(compassHousing).y;
+            currentHeading = 0;
+            needleAngle = 0;
+            if (!buildCompassFace()) {
+                return;
+            }
+            drawCompassFace();
+            drawCompassNeedle();
+            hasWebkit();
+        }
+        function buildCompassFace() {
+            // compass housing diameter and radius
+            compassFaceDiameter = COMPASS_SIZE;
+            compassFaceRadius = compassFaceDiameter / 2;
+            // needle length
+            needleLength = compassFaceDiameter;
+            // needle width
+            needleWidth = needleLength / 10;
+            // tick marks
+            compassRing = compassFaceDiameter / 50;
+            return true;
+        }
+        var compassFaceContext;
+        // Draw the coppass face, text labels and font, and tick marks
+        function drawCompassFace() {
+            var compassFaceCanvas = dom.byId("compassFace");
+            compassFaceCanvas.width = compassFaceCanvas.height = compassFaceDiameter;
+            compassFaceContext = compassFaceCanvas.getContext("2d");
+            compassFaceContext.clearRect(0, 0, compassFaceCanvas.width, compassFaceCanvas.height);
+
+            // draw the tick marks and center the compass ring
+            var xOffset, yOffset;
+            xOffset = yOffset = compassFaceCanvas.width / 2;
+            for (var i = 0; i < 360; ++i) {
+                var x = (compassFaceRadius * Math.cos(degToRad(i))) + xOffset;
+                var y = (compassFaceRadius * Math.sin(degToRad(i))) + yOffset;
+                var x2 = ((compassFaceRadius - compassRing) * Math.cos(degToRad(i))) + xOffset;
+                var y2 = ((compassFaceRadius - compassRing) * Math.sin(degToRad(i))) + yOffset;
+                compassFaceContext.beginPath();
+                compassFaceContext.moveTo(x, y);
+                compassFaceContext.lineTo(x2, y2);
+                compassFaceContext.closePath();
+                compassFaceContext.stroke();
+                i = i + 4;
+            }
+            // The measureText method returns an object, with one attribute: width.
+            // The width attribute returns the width of the text, in pixels.
+            compassFaceContext.font = "10px Arial";
+            compassFaceContext.textAlign = "center";
+            var metrics = compassFaceContext.measureText('N');
+            compassFaceContext.fillText('N', compassFaceRadius, 15);
+            compassFaceContext.fillText('S', compassFaceRadius, compassFaceDiameter - 10);
+            compassFaceContext.fillText('E', (compassFaceRadius + (compassFaceRadius - metrics.width)), compassFaceRadius);
+            compassFaceContext.fillText('W', 10, compassFaceRadius);
+        }
+        // Draw the compass needle
+        function drawCompassNeedle() {
+            var compassNeedle = dom.byId("compassNeedle");
+            compassNeedle.width = compassNeedle.height = compassFaceDiameter;
+            compassNeedle.style.left = Math.floor(compassFaceContext.width / 2) + "px";
+            compassNeedle.style.top = Math.floor(compassFaceContext.height / 2) + "px";
+            compassNeedleContext = compassNeedle.getContext("2d");
+            compassNeedleContext.translate(compassFaceRadius, compassFaceRadius);
+            compassNeedleContext.clearRect((compassNeedleContext.canvas.width / 2) * -1, (compassNeedleContext.canvas.height / 2) * -1, compassNeedleContext.canvas.width, compassNeedleContext.canvas.height);
+
+            // The first step to create a path is calling the beginPath method. Internally, paths are stored as a list of sub-paths
+            // (lines, arcs, etc) which together form a shape. Every time this method is called, the list is reset and we can start
+            // drawing new shapes.
+
+            // SOUTH
+            compassNeedleContext.beginPath();
+            compassNeedleContext.lineWidth = 1;
+            compassNeedleContext.moveTo(0, 5);
+            compassNeedleContext.lineTo(0, compassFaceRadius);
+            compassNeedleContext.stroke();
+            // circle around label
+            compassNeedleContext.beginPath();
+            compassNeedleContext.arc(0, compassFaceRadius - 15, 8, 0, 2 * Math.PI, false);
+            compassNeedleContext.fillStyle = "#FFF";
+            compassNeedleContext.fill();
+            compassNeedleContext.lineWidth = 1;
+            compassNeedleContext.strokeStyle = "black";
+            compassNeedleContext.stroke();
+            // S
+            compassNeedleContext.beginPath();
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.font = "normal 10px Verdana";
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.textAlign = "center";
+            compassNeedleContext.fillText("S", 0, compassFaceRadius - 10);
+            // needle
+            compassNeedleContext.beginPath();
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.lineTo(0, needleLength / 4);
+            compassNeedleContext.lineTo((needleWidth / 4) * -1, 0);
+            compassNeedleContext.fill();
+            compassNeedleContext.beginPath();
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.lineTo(0, needleLength / 4);
+            compassNeedleContext.lineTo(needleWidth / 4, 0);
+            compassNeedleContext.fill();
+
+
+            // NORTH
+            compassNeedleContext.beginPath();
+            compassNeedleContext.lineWidth = 1;
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.lineTo(0, - compassFaceRadius);
+            compassNeedleContext.stroke();
+            // circle
+            compassNeedleContext.beginPath();
+            compassNeedleContext.arc(0, - (compassFaceRadius - 16), 8, 0, 2 * Math.PI, false);
+            compassNeedleContext.fillStyle = "#FFF";
+            compassNeedleContext.fill();
+            compassNeedleContext.lineWidth = 1;
+            compassNeedleContext.strokeStyle = "black";
+            compassNeedleContext.stroke();
+            // N
+            compassNeedleContext.beginPath();
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.font = "normal 10px Verdana";
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.textAlign = "center";
+            compassNeedleContext.fillText("N", 0, - (compassFaceRadius - 20));
+            // needle
+            compassNeedleContext.beginPath();
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.lineTo(0, (needleLength / 4) * -1);
+            compassNeedleContext.lineTo((needleWidth / 4) * -1, 0);
+            compassNeedleContext.fill();
+            compassNeedleContext.beginPath();
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.lineTo(0, (needleLength / 4) * -1);
+            compassNeedleContext.lineTo(needleWidth / 4, 0);
+            compassNeedleContext.fill();
+
+            // center pin color
+            compassNeedleContext.beginPath();
+            compassNeedleContext.arc(0, 0, 10, 0, 2 * Math.PI, false);
+            compassNeedleContext.fillStyle = "rgb(255,255,255)";
+            compassNeedleContext.fill();
+            compassNeedleContext.lineWidth = 1;
+            compassNeedleContext.strokeStyle = "black";
+            compassNeedleContext.stroke();
+
+            compassNeedleContext.beginPath();
+            compassNeedleContext.moveTo(0, 0);
+            compassNeedleContext.arc(0, 0, (needleWidth / 4), 0, degToRad(360), false);
+            compassNeedleContext.fillStyle = "#000";
+            compassNeedleContext.fill();
+        }
+        var orientationHandle;
+        function orientationChangeHandler() {
+            // An event handler for device orientation events sent to the window.
+            orientationHandle = on(window, "deviceorientation", onDeviceOrientationChange);
+            // The setInterval() method calls rotateNeedle at specified intervals (in milliseconds).
+            renderingInterval = setInterval(rotateNeedle, 100);
+        }
+
+        var compassTestHandle;
+        function hasWebkit() {
+            if (has("ff") || has("ie") || has("opera")) {
+                hasCompass = false;
+                orientationChangeHandler();
+                alert("Your browser does not support WebKit.");
+            } else if (window.DeviceOrientationEvent) {
+                compassTestHandle = on(window, "deviceorientation", hasGyroscope);
+            } else {
+                hasCompass = false;
+                orientationChangeHandler();
+            }
+        }
+
+        // Test if the device has a gyroscope.
+        // Instances of the DeviceOrientationEvent class are fired only when the device has a gyroscope and while the user is changing the orientation.
+        function hasGyroscope(event) {
+            dojo.disconnect(compassTestHandle);
+            if (event.webkitCompassHeading !== undefined || event.alpha != null) {
+                hasCompass = true;
+            } else {
+                hasCompass = false;
+            }
+            orientationChangeHandler();
+        }
+
+        // Rotate the needle based on the device's current heading
+        function rotateNeedle() {
+            var multiplier = Math.floor(needleAngle / 360);
+            var adjustedNeedleAngle = needleAngle - (360 * multiplier);
+            var delta = currentHeading - adjustedNeedleAngle;
+            if (Math.abs(delta) > 180) {
+                if (delta < 0) {
+                    delta += 360;
+                } else {
+                    delta -= 360;
+                }
+            }
+            delta /= 5;
+            needleAngle = needleAngle + delta;
+            var updatedAngle = needleAngle - window.orientation;
+            // rotate the needle
+            dom.byId("compassNeedle").style.webkitTransform = "rotate(" + updatedAngle + "deg)";
+        }
+
+        function onDeviceOrientationChange(event) {
+            var accuracy;
+            if (event.webkitCompassHeading !== undefined) {
+                // Direction values are measured in degrees starting at due north and continuing clockwise around the compass.
+                // Thus, north is 0 degrees, east is 90 degrees, south is 180 degrees, and so on. A negative value indicates an invalid direction.
+                currentHeading = (360 - event.webkitCompassHeading);
+                accuracy = event.webkitCompassAccuracy;
+            } else if (event.alpha != null) {
+                // alpha returns the rotation of the device around the Z axis; that is, the number of degrees by which the device is being twisted
+                // around the center of the screen
+                // (support for android)
+                currentHeading = (270 - event.alpha) * -1;
+                accuracy = event.webkitCompassAccuracy;
+            }
+
+            if (accuracy < 11) {
+                compassNeedleContext.fillStyle = "rgba(0, 205, 0, 0.9)";
+            } else if (accuracy >= 15 && accuracy < 25) {
+                compassNeedleContext.fillStyle = "rgba(255, 255, 0, 0.9)";
+            } else if (accuracy > 24) {
+                compassNeedleContext.fillStyle = "rgba(255, 0, 0, 0.9)";
+            }
+            compassNeedleContext.fill();
+
+            if (renderingInterval == -1) {
+                rotateNeedle();
+            }
+        }
+
+        // Convert degrees to radians
+        function degToRad(deg) {
+            return (deg * Math.PI) / 180;
+        }
+        // Handle portrait and landscape mode orientation changes
+        function orientationChanged() {
+            if (map) {
+                map.reposition();
+                map.resize();
+            }
+        }
 
         //单击获取map页面的点
         function getPoint(evt){
