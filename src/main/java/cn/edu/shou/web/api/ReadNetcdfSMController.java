1d5de5dac17454d1979c2e8e8f15c94770d98de2
Allen_Y <626668645@qq.com>
2016-04-24 11:09:28 +0800

    modify Algorithm
    modify Algorithm

--- src/main/java/cn/edu/shou/web/api/ReadNetcdfSMController.java
+++ src/main/java/cn/edu/shou/web/api/ReadNetcdfSMController.java
@@ -25,14 +25,14 @@ public class ReadNetcdfSMController {
     String filename = "D:\\jidi\\SM_20160420_05h48m.nc";
     //获取u v 数据值
     // lat 纬度 lon 经度
-    public List<Map<String, String>> getNetCdfPredictData(int lat,int lon) throws Exception{
+    public List<Map<String, String>> getNetCdfPredictData() throws Exception{
         List<Map<String,String>> list = new ArrayList<Map<String,String>>();
         try{
             ncfile = NetcdfFile.open(filename);
-            String variable = "u10";
-            String variable10 = "v10";
-            String lats ="lat";
-            String lons ="lon";
+            String variable = "U_10m";
+            String variable10 = "V_10m";
+            String lats ="latitude";
+            String lons ="longitude";
             Variable varlat =ncfile.findVariable(lats);
             Variable varlon =ncfile.findVariable(lons);
             Variable varu10 = ncfile.findVariable(variable);
@@ -44,8 +44,7 @@ public class ReadNetcdfSMController {
                 //第二个参数为经度的数据编号，表示从哪个经度数据开始
                 //第三个参数为维度的数据编号，表示从哪个维度数据开始
                 int[] origin = new int[]{0, 0, 0};//位置
-                origin[1]=lat;
-                origin[2]=lon;
+
                 //第一个参数表示时间的范围，72表示要读取72个时刻的数据
                 //第二个表示经度的数据范围，1表示只读去一个点的数据
                 //第三个表示纬度的数据范围，1表示只读一个点的数据
@@ -57,8 +56,10 @@ public class ReadNetcdfSMController {
                 //经纬度的值
                 Array arrLat = varlat.read("0:170:5");
                 Array arrLon = varlon.read("0:116:5");
+
+
                 //开方
-                for(int i = 0;i<24;i++){
+               /* for(int i = 0;i<24;i++){
                     Map<String,String> map =new HashMap();
                     Double netcdfSqrt = Math.sqrt((Math.pow(data2D.getDouble(i), 2) + Math.pow(data3D.getDouble(i), 2)));
                     System.out.println("netcdf"+netcdfSqrt);
@@ -68,7 +69,7 @@ public class ReadNetcdfSMController {
                         map.put("windDir",String.valueOf(arrLon.getDouble(i)));
                         list.add(map);
                     }
-                }
+                }*/
             }
         }catch (Exception e){
             e.printStackTrace();
@@ -82,14 +83,13 @@ public class ReadNetcdfSMController {
         }
         return list;
     }
-    @RequestMapping(value = "/PostCoordinates/{lat}/{lon}",method =RequestMethod.GET)
-    public  List<tbjhshipdata> postCoordiantes( @PathVariable int lat,
-                                  @PathVariable int lon){
+    @RequestMapping(value = "/PostSMCoordinates/",method =RequestMethod.GET)
+    public  List<tbjhshipdata> postCoordiantes(){
         List<Map<String,String>> list1 = new ArrayList<Map<String,String>>();
         try {
-            Map<String,Integer> map=getLatAndLonIndex(lat,lon);
+          // Map<String,Integer> map=getLatAndLonIndex();
             //return list
-            list1 =  getNetCdfPredictData(map.get("latindex"),map.get("lonindex"));
+            list1 =  getNetCdfPredictData();
             System.out.println("list1 is"+list1);
         }catch (Exception e){
             e.printStackTrace();
@@ -104,10 +104,10 @@ public class ReadNetcdfSMController {
         return results;
     }
 
-    private Map<String,Integer>getLatAndLonIndex(int lat,int lon){
+/*    private Map<String,Integer>getLatAndLonIndex(int lat,int lon){
         Map<String,Integer> latAndLonIndex =new HashMap();//接收鼠标点击经纬度对应nc文件的编号
         latAndLonIndex.put("latindex",lat);
         latAndLonIndex.put("lonindex",lon);
         return latAndLonIndex;
-    }
+    }*/
 }
